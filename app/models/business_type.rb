e9d7f5f1653680620c6a5ab1fb916a29808c777f
Joel Mahoney <joelmahoney@gmail.com>
2013-04-13 00:34:24 -0700

    Remove business_type from Organization

--- app/models/business_type.rb
+++ app/models/business_type.rb
@@ -1,7 +1,6 @@
 class BusinessType < ActiveRecord::Base
   attr_accessible :name
   has_and_belongs_to_many :locations
-  has_many :organizations
   
   def results_in_set(locations_set)
     (self.locations & locations_set).try(:size)
