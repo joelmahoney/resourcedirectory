f20744dbcc45065285cd1676c34fc0a9500018e6
Joel Mahoney <joelmahoney@gmail.com>
2013-05-31 01:12:16 -0700

    Turn of geocoding

--- app/models/location.rb
+++ app/models/location.rb
@@ -7,8 +7,8 @@ class Location < ActiveRecord::Base
   has_and_belongs_to_many :business_types, :uniq => true
   
   
-  acts_as_gmappable :address => :full_address
-  geocoded_by :full_address
+  # acts_as_gmappable :address => :full_address
+  # geocoded_by :full_address
     
   attr_accessible :ac_bus_accessible, :address, :bart_accessible, :city, :directions, :email, :fax, :free_parking, :free_street_parking, :hours, :latitude, :longitude, :muni_bus_accessible, :muni_train_accessible, :name, :organization_id, :paid_parking_lot, :parking_fees, :parking_meters, :phone, :public_transportation_stop, :wheelchair_accessible, :zipcode, :service_ids, :service_delivery_option_ids, :business_type_ids, :delivery_method
   
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,7 +2,7 @@ class Organization < ActiveRecord::Base
   has_many :locations, :dependent => :destroy, :order => :address
   has_and_belongs_to_many :users, :uniq => true
 
-  geocoded_by :full_address  
+  # geocoded_by :full_address
   
   attr_accessible :address, :city, :description, :email, :latitude, :longitude, :name, :phone, :population, :website, :zipcode, :contact
   
